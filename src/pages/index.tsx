8d0357a5c8778d7e6fb924eebf8671e7a52ebe83
AlexArchibeque <wonkman33@gmail.com>
2022-12-08 16:12:51 -0800

    cleanup of errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
 import { type NextPage } from "next";
 import { RotatingSection } from "../components/mainPageRotatingSection/rotatingSection";
-import { useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
-  // const { data: session, status } = useSession();
-  {
-    /* {status === "loading" ? "Loading ..." : "In the main page"} */
-  }
-
   return (
     <main className="flex flex-col items-center">
       In the main page
