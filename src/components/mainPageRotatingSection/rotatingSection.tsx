7a21b0639878d0e0eea366f88c8ac536b0c2aa81
AlexArchibeque <wonkman33@gmail.com>
2022-12-08 16:14:04 -0800

    fixed escape errors

--- src/components/mainPageRotatingSection/rotatingSection.tsx
+++ src/components/mainPageRotatingSection/rotatingSection.tsx
@@ -8,8 +8,8 @@ export const RotatingSection = () => {
     <>
       <div className="flex min-w-full items-center">
         <div className="flex min-w-full justify-between">
-          <button className="bg-slate-700">{`<`} LEFT ARROW</button>
-          <button className="bg-slate-700">{`>`} RIGHT ARROW</button>
+          <button className="bg-slate-700">LEFT ARROW</button>
+          <button className="bg-slate-700">RIGHT ARROW</button>
         </div>
         <div className="top-0 left-0 flex min-h-[300px] min-w-[400px] bg-red-700">
           <IndividualSection classes="relative z-0 left-4" />
