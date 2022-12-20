a22b8bc28696a858cc5f4ada8345b117a5c427cb
AlexArchibeque <wonkman33@gmail.com>
2022-12-20 11:32:15 -0800

    cloudinary set up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
 import { type NextPage } from "next";
 import { RotatingSection } from "../components/mainPageRotatingSection/rotatingSection";
+import { CldImage } from "next-cloudinary";
 
 const Home: NextPage = () => {
   return (
     <main className="flex flex-col items-center">
       <RotatingSection />
+      <CldImage
+        width={144}
+        height={192}
+        src="categories/Super_Metroid_pfg8lq.jpg"
+        alt="Example"
+      />
       <div>Live channels we think you`ll like</div>
       <div>Categories we think you`ll like</div>
     </main>
